b81c8ded6b2786d850cfdb33fec4cada551c41c5
Nabin Khanal <nabinkhanal@skiff.com>
2023-12-06 00:15:56 +0545

    barebones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1>Image Inpainting using Encoder Decoder Network with Partial Convolution</h1>
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold">Image Inpainting using Encoder Decoder Network with Partial Convolution</h1>
+    </div>
   );
 }
 
