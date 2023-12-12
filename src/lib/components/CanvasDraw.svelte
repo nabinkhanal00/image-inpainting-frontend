cd043a2202db35cbf7d4f8438ec89b193c8ea723
Nabin Khanal <nabinkhanal@skiff.com>
2023-12-12 17:25:00 +0545

    added bushing component

--- src/lib/components/CanvasDraw.svelte
+++ src/lib/components/CanvasDraw.svelte
@@ -3,15 +3,120 @@
 
   export let width = 300;
   export let height = 300;
-  export let color = "#333";
+  export let brushColor = "#ffffff";
+  export let brushRadius;
   export let imgSrc;
 
   let imageCanvas, imageContext;
   let maskCanvas, maskContext;
-  let isDrawing;
-  let start;
-  console.log("Image Source:", imgSrc);
+  let isDrawing = false;
+  let isPressing = false;
+  let mouseHasMoved = true;
+  let valuesChanged = true;
+  export let showMask = true;
 
+  let start;
+  let points = [],
+    lines = [];
+  const midPoint = (p1, p2) => {
+    return {
+      x: p1.x + (p2.x - p1.x) / 2,
+      y: p1.y + (p2.y - p1.y) / 2,
+    };
+  };
+  const handleStart = (e) => {
+    e.preventDefault();
+    isPressing = true;
+    const { x, y } = getPointerPos(e);
+    handlePointerMove(x, y);
+  };
+  let getPointerPos = (e) => {
+    const rect = maskCanvas.getBoundingClientRect();
+    let clientX = e.clientX;
+    let clientY = e.clientY;
+
+    if (e.changedTouches && e.changedTouches.length > 0) {
+      clientX = e.changedTouches[0].clientX;
+      clientY = e.changedTouches[0].clientY;
+    }
+    return {
+      x: clientX - rect.left,
+      y: clientY - rect.top,
+    };
+  };
+  let handlePointerMove = (x, y) => {
+    if (isPressing && !isDrawing) {
+      isDrawing = true;
+      points.push({ x, y });
+    }
+    if (isDrawing) {
+      points.push({ x, y });
+      drawPoints({ points: points, brushRadius: brushRadius });
+    }
+    mouseHasMoved = true;
+  };
+  let drawPoints = ({ points, brushRadius }) => {
+    if (!showMask) {
+      return;
+    }
+    if (points.length < 2) {
+      return;
+    }
+    maskContext.lineJoin = "round";
+    maskContext.lineCap = "round";
+    maskContext.lineWidth = brushRadius;
+    maskContext.strokeStyle = brushColor;
+    let p1 = points[0];
+    let p2 = points[1];
+    maskContext.moveTo(p2.x, p2.y);
+    maskContext.beginPath();
+    for (let i = 1, len = points.length; i < len; i++) {
+      let mp = midPoint(p1, p2);
+      maskContext.quadraticCurveTo(p1.x, p1.y, mp.x, mp.y);
+      p1 = points[i];
+      p2 = points[i + 1];
+    }
+    maskContext.lineTo(p1.x, p1.y);
+    maskContext.stroke();
+  };
+
+  let handleMove = (e) => {
+    e.preventDefault();
+    const { x, y } = getPointerPos(e);
+    handlePointerMove(x, y);
+  };
+  let handleEnd = (e) => {
+    e.preventDefault();
+    handleMove(e);
+    if (isPressing) {
+      saveLine({ brushRadius });
+    }
+    isDrawing = false;
+    isPressing = false;
+  };
+  let saveLine = ({ radius } = {}) => {
+    lines.push({ points: [...points], brushRadius: radius || brushRadius });
+    points.length = 0;
+  };
+  export const toggleMask = () => {
+    showMask = !showMask;
+    redraw();
+  };
+  export const clearMask = () => {
+    lines.length = 0;
+    redraw();
+  };
+  export const undoMask = () => {
+    lines = lines.slice(0, -1);
+    redraw();
+  };
+  const redraw = () => {
+    maskContext.clearRect(0, 0, width, height);
+    lines.forEach((line) => {
+      const { points, brushRadius } = line;
+      drawPoints({ points, brushRadius });
+    });
+  };
   onMount(() => {
     imageContext = imageCanvas.getContext("2d");
     const img = new Image();
@@ -21,76 +126,46 @@
     };
   });
 
-  // const handleStart = ({ offsetX: x, offsetY: y }) => {
-  //   if (color === background) {
-  //     context.clearRect(0, 0, width, height);
-  //   } else {
-  //     isDrawing = true;
-  //     start = { x, y };
-  //   }
-  // };
-
-  // const handleEnd = () => {
-  //   isDrawing = false;
-  // };
-  // const handleMove = ({ offsetX: x1, offsetY: y1 }) => {
-  //   if (!isDrawing) return;
-
-  //   const { x, y } = start;
-  //   context.beginPath();
-  //   context.moveTo(x, y);
-  //   context.lineTo(x1, y1);
-  //   context.closePath();
-  //   context.stroke();
-
-  //   start = { x: x1, y: y1 };
-  // };
-
-  // const handleSize = () => {
-  //   const { top, left } = canvas.getBoundingClientRect();
-  //   t = top;
-  //   l = left;
-  // };
+  onMount(() => {
+    maskContext = maskCanvas.getContext("2d");
+  });
 </script>
 
-<canvas id="image" bind:this={imageCanvas} {width} {height} />
+<div id="canvasContainer">
+  <canvas id="image" bind:this={imageCanvas} {width} {height} />
 
-<!--
-<canvas
-  id="maskCanvas"
-  bind:this={maskCanvas}
-  {width}
-  {height}
-  on:mousedown={handleStart}
-  on:touchstart={(e) => {
-    const { clientX, clientY } = e.touches[0];
-    handleStart({
-      offsetX: clientX - l,
-      offsetY: clientY - t,
-    });
-  }}
-  on:mouseup={handleEnd}
-  on:touchend={handleEnd}
-  on:mouseleave={handleEnd}
-  on:mousemove={handleMove}
-  on:touchmove={(e) => {
-    const { clientX, clientY } = e.touches[0];
-    handleMove({
-      offsetX: clientX - l,
-      offsetY: clientY - t,
-    });
-  }}
-/>
--->
+  <canvas
+    id="mask"
+    bind:this={maskCanvas}
+    {width}
+    {height}
+    on:mousedown={handleStart}
+    on:mousemove={handleMove}
+    on:mouseup={handleEnd}
+    on:mouseleave={handleEnd}
+    on:touchstart={handleStart}
+    on:touchmove={handleMove}
+    on:touchend={handleEnd}
+    on:touchcancel={handleEnd}
+  />
+</div>
+
 <style>
-  #image {
+  #canvasContainer {
     position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+  #image {
     z-index: 1;
     margin-left: auto;
     margin-right: auto;
   }
   #mask {
+    align-self: center;
     position: absolute;
+    margin-left: auto;
+    margin-right: auto;
     z-index: 2;
   }
 </style>
--- src/lib/components/FileAnnotator.svelte
+++ src/lib/components/FileAnnotator.svelte
@@ -1,27 +1,43 @@
 <script>
+  import { onMount } from "svelte";
   import CanvasDraw from "./CanvasDraw.svelte";
   import { RangeSlider } from "@skeletonlabs/skeleton";
   export let url, classes, width, height, changeImage;
-  let brushRadius = 5;
+  let brushRadius = 1;
   export let brushMax = 10;
   let brushColor = "white";
-  const hideMask = () => {};
-  const clearMask = () => {};
-  const undoMask = () => {};
+  let comp;
+  const toggleMask = () => {
+    comp.toggleMask();
+  };
+  const clearMask = () => {
+    comp.clearMask();
+  };
+  const undoMask = () => {
+    comp.undoMask();
+  };
   const applyModel = () => {};
+  let showMask = true;
+  let slider;
+  onMount(() => {
+    slider.style.width = width + "px";
+  });
 </script>
 
 <div class=" {classes}">
   <CanvasDraw
+    bind:this={comp}
+    {brushRadius}
     {width}
     {height}
     classes="self-center"
     imgSrc={url}
     {brushColor}
-    {brushRadius}
+    bind:showMask
+
   />
 
-  <div class="w-[{Math.floor(width)}px] ml-auto mr-auto py-2">
+  <div bind:this={slider} class=" ml-auto mr-auto py-2">
     <RangeSlider
       name="brush-size"
       bind:value={brushRadius}
@@ -39,7 +55,7 @@
     <button class="btn variant-ringed" on:click={changeImage}
       >Change Image</button
     >
-    <button class="btn variant-ringed" on:click={hideMask}>Hide Mask</button>
+    <button class="btn variant-ringed" on:click={toggleMask}>{showMask? "Hide Mask" : "Show Mask"}</button>
     <button class="btn variant-ringed" on:click={clearMask}>Clear</button>
     <button class="btn variant-ringed" on:click={undoMask}>Undo</button>
     <button class="btn variant-filled" on:click={applyModel}>Apply Model</button
